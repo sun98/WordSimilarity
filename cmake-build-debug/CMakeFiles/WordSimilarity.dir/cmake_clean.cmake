11a4484efe3676b7efc5d04209f7da2a28ec503f
Nibius <1329575336@qq.com>
2017-12-19 23:26:53 +0800

    init commit

---
+++ cmake-build-debug/CMakeFiles/WordSimilarity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WordSimilarity.dir/similarity.cpp.o"
+  "WordSimilarity.pdb"
+  "WordSimilarity"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WordSimilarity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
